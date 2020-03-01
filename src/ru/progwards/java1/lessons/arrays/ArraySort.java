98fa5c6ef8e4d11f675051841fc862b88f81793d
vesel <v.e.seleznev@mail.ru>
2020-03-01 09:26:13 +0800

    HelloWorld start

---
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -0,0 +1,24 @@
+package ru.progwards.java1.lessons.arrays;
+
+public class ArraySort {
+    public static void sort(int[] a) {
+        int stackArray;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    stackArray = a[i];
+                    a[i] = a[j];
+                    a[j] = stackArray;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrayResult = {9, 1214, 0, 3, 10, 74, 25, -3, 44, 37, 5};
+        sort(arrayResult);
+        for (int i = 0; i < arrayResult.length; i++) {
+            System.out.println(arrayResult[i]);
+        }
+    }
+}
