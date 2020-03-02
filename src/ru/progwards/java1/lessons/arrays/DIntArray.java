d56a3d32aac7792e7b1f926ed5a06f358e607a83
vesel <v.e.seleznev@mail.ru>
2020-03-03 01:01:25 +0800

    HelloWorld start

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -2,37 +2,40 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class DIntArray {
-    private int aArray[] = {1, 2, 3, 4, 5};
+    int aArray[] = new int[0];
 
     public DIntArray() {
     }
 
     public void add(int num) {
-        int bArray[] = Arrays.copyOf(aArray, aArray.length + 1);
-        bArray[bArray.length - 1] = num;
-        //System.out.println(Arrays.toString(bArray));
+        aArray = Arrays.copyOf(aArray, aArray.length + 1);
+        aArray[aArray.length - 1] = num;
     }
     public void atInsert(int pos, int num) {
-        int bArray[] = Arrays.copyOf(aArray, aArray.length + 1);
-        for (int i = bArray.length - 2; i + 1 > pos; i--) {
-            bArray[i + 1] = bArray[i];
+        aArray = Arrays.copyOf(aArray, aArray.length + 1);
+        for (int i = aArray.length - 2; i + 1 > pos; i--) {
+            aArray[i + 1] = aArray[i];
         }
-        bArray[pos] = num;
-        //System.out.println(Arrays.toString(bArray));
+        aArray[pos] = num;
     }
     public void atDelete(int pos) {
         for (int i = pos; i < aArray.length - 1; i++) {
             aArray[i] = aArray[i + 1];
         }
-        int bArray[] = Arrays.copyOf(aArray, aArray.length - 1);
-        //System.out.println(Arrays.toString(bArray));
+        aArray = Arrays.copyOf(aArray, aArray.length - 1);
     }
 
     public static void main(String[] args) {
         DIntArray stringArray = new DIntArray();
         stringArray.add(12);
-        //System.out.println(Arrays.toString(stringArray.bArray));
+        System.out.println(Arrays.toString(stringArray.aArray));
+        stringArray.add(134);
+        System.out.println(Arrays.toString(stringArray.aArray));
         stringArray.atInsert(1, 15);
+        System.out.println(Arrays.toString(stringArray.aArray));
+        stringArray.atInsert(2, 9);
+        System.out.println(Arrays.toString(stringArray.aArray));
         stringArray.atDelete(1);
+        System.out.println(Arrays.toString(stringArray.aArray));
     }
 }
