be17f62d1724c541867fa0d220045d632d57dd93
vesel <v.e.seleznev@mail.ru>
2020-03-04 07:15:16 +0800

    HelloWorld start

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,18 +2,19 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static int sumBits(byte value) {
-        // определяем переменную которая будет выводить сумму едениц
-        byte sumNumber = 0;
-        while (value > 0) {
-            byte intermediateValue = (byte) (value & 0b1); // устанавливаем есть ли в конце числа еденица
+        int uvalue = value & 0b11111111;
+        int sumDigit = 0;
+        System.out.println(Integer.toBinaryString(uvalue));
+        for (int i = 0; i < 8; i++) {
+            int intermediateValue = uvalue & 0b1; // устанавливаем есть ли в конце числа еденица
             if (intermediateValue > 0) // если есть
-                sumNumber++; // ссумируем к исходной переменной
-            value >>= 1; // смещаем число вправо для повторения данного цикла со следующей цифрой числа
+                sumDigit++;// прибавляем единицу к сумме
+            uvalue >>= 1; // смещаем число вправо для повторения данного цикла со следующей цифрой числа
         }
-        return sumNumber;
+        return sumDigit;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b010111));
+        System.out.println(sumBits((byte) -128));
     }
 }
