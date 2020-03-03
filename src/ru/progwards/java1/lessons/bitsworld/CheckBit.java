0fcc0dc1c10f126dfe42a51fcdded11f23a55e27
vesel <v.e.seleznev@mail.ru>
2020-03-03 13:21:43 +0800

    HelloWorld start

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -1,4 +1,17 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
+    public static int checkBit(byte value, int bitNumber) {
+        int bit = 0;
+        while (bitNumber >= 0) {
+            bit = value & 0b1;
+            value >>= 1;
+            bitNumber--;
+        }
+        return bit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkBit((byte) 0b1011001, 0));
+    }
 }
